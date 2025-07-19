b752c775ba9758e54daba1bc490ea051c94a8e62
ayman644 <ayymann.s@gmail.com>
2025-07-19 11:26:03 +1000

    added ekta web project

--- src/compenents/sections/Project.jsx
+++ src/compenents/sections/Project.jsx
@@ -74,6 +74,38 @@ export const Projects = () => {
                             </div>
                             
                         </div>
+
+                        <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-purple-500/30 hover:shadow-[0_2px_8px_rbga(40,0,43,0.2)] transition
+                                        bg-white/5 backdrop-blur-md">
+                            <h3 className="text-xl font-bold mb-2"> ANU Ekta Website </h3>
+                            <p className="text-gray-200 mb-4 text-sm"> 
+                                    A modern, responsive website built for Ekta, ANU's South Asian cultural society. Developed using React, TypeScript and Tailwind CSS. Deployed on Vercel with a custom domain: 
+                                    <a className="font-bold"href="https://www.anuekta.com" target="_blank">"anuekta.com"</a>.
+                            </p>
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {["React", "TypeScript", "HTML/CSS", "Tailwindcss","Supabase", "Python", "API", "Vercel"].map((tech, key) => (
+                                    <span 
+                                    key={key}
+                                    className="bg-purple-500/10 text-purple-500 py-1 px-3 rounded-full text-sm hover:bg-purple-500/20
+                                    hover:shadow-[0_2px_8px_rbga(40,0,43,0.2)] transition-all">
+                                    {tech}
+                                    </span>
+                                ))}
+                            </div>
+                            <div className="flex items-center gap-2 my-4">
+                            
+                                <a
+                                href="https://github.com/ayman644/EktaWeb"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex items-center gap-2 text-purple-400 hover:text-purple-300 transition-colors my-4
+                                            text-sm"> 
+                                View Project →
+                                <FaGithub size={22} />
+                                </a>
+                            </div>
+                            
+                        </div>
                     </div>
                 </div>
             </RevealOnScroll>
